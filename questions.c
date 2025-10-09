fb6b234d5c5d7c6c57935e062a2d831206ba2f87
siyarampatel <siyarampateljbp@gmail.com>
2025-10-09 18:04:03 +0530

    Update questions.c

--- questions.c
+++ questions.c
@@ -1,4 +1,30 @@
+//Q1=>Find the maximum and minimum element in an array        
+
 #include<stdio.h>
 int main(){
-  printf("Practice Questions")
+    printf("Enter size of array: ");
+     int n;
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter elements in array:");
+    for(int i=0; i<n; i++){
+        scanf("%d",&a[i]);
+    }
+    int max = a[0];
+    for(int i=0; i<n; i++) {
+        if(a[i]>max){
+            max = a[i];
+        }
+        
+    }
+    printf("The maximum element is %d\n",max);
+    int min = a[0];
+    for(int i=0; i<n; i++) {
+        if(a[i]<min){
+            min = a[i];
+        }
+        
+    }
+    printf("The minimum element is %d",min);
+
 }
