9866914eb86e7afc5f3afa0e67bc6330a1ccff24
siyarampatel <siyarampateljbp@gmail.com>
2025-10-17 15:34:18 +0530

    Update questions.c

--- questions.c
+++ questions.c
@@ -225,3 +225,24 @@ printf("The intersection of arrays is: ");
     }
     printf("\n");
 }
+
+//Q7 => Cyclically rotate an array by one
+#include<stdio.h>
+int main(){
+    int arr [111];
+    int n;
+    printf("Enter no. of elements:");
+    scanf("%d",&n);
+    printf("Enter %d elements\n",n);
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int temp = arr[n-1];
+    for(int i=n-1; i>0; i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
+    for(int i=0; i<n; i++){
+        printf("%d ",arr[i]);
+    }
+}
