1c577d51ec243e78f85bdb9bc590888816e0c66f
siyarampatel <siyarampateljbp@gmail.com>
2025-10-17 15:50:22 +0530

    Update questions.c

--- questions.c
+++ questions.c
@@ -237,7 +237,7 @@ int main(){
     for(int i=0; i<n; i++){
         scanf("%d",&arr[i]);
     }
-    int temp = arr[n-1];
+    int temp = arr[n-1];      // storing last element in an variable to prevent this arr[0]=arr[-1]
     for(int i=n-1; i>0; i--){
         arr[i]=arr[i-1];
     }
