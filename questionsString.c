4f6cdbb1c9c4ca230ce376e098845192f8f963e4
siyarampatel <siyarampateljbp@gmail.com>
2025-10-27 10:45:35 +0530

    Add string reversal functionality
    Implemented a program to reverse a string input by the user.

--- questionsString.c
+++ questionsString.c
@@ -1,4 +1,16 @@
+//Q1=>Reverse a string
 #include<stdio.h>
 int main(){
-printf("String");
+    char str[20];
+    char str1[20];
+    printf("Enter your string: ");
+    scanf("%s",str);
+   
+    int n = strlen(str);
+    for(int i=0; str[i]!='\0'; i++){
+        str1[i]=str[n-i-1];
+    }
+    str1[n]='\0';
+    
+    printf("%s",str1);
 }
